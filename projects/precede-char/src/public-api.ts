f860c89c90ed2cc097b96c4f1fdea33616da5632
linch90 <zlchu@outlook.com>
2022-11-18 12:24:10 +0800

    Initial commit.

---
+++ projects/precede-char/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of precede-char
+ */
+
+export * from './lib/precede-char.service';
+export * from './lib/precede-char.component';
+export * from './lib/precede-char.module';
