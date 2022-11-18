8be0a5f4a94aad84035bc93c5317fb441e0c4f5b
linch90 <zlchu@outlook.com>
2022-11-18 12:34:50 +0800

    renaming precede-char to precede-char-directive

---
+++ projects/precede-char-directive/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of precede-char-directive
+ */
+
+export * from './lib/precede-char-directive.service';
+export * from './lib/precede-char-directive.component';
+export * from './lib/precede-char-directive.module';
--- projects/precede-char/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of precede-char
- */
-
-export * from './lib/precede-char.service';
-export * from './lib/precede-char.component';
-export * from './lib/precede-char.module';
